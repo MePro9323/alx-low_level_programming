e85e0021b9c86f496c985463587a1dbfeca0ce20
MePro9323 <mabarbi@esca.ma>
2023-09-03 09:20:59 -0700

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: Pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; 1 < n; i++)
-		s[i] = b;
-	return (s);
+for (i = 0; 1 < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
 }
