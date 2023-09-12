11239e0a989aae242cdd43fccb48600f6ac1fb98
MePro9323 <mabarbi@esca.ma>
2023-09-12 12:53:18 -0700

    0. Multiple mains

---
+++ 3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *s)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
